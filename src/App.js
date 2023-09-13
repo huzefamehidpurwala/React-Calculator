61fe2bfcd0d100a134f69825a79a593f10b8f9a4
huzefamehidpurwala <jamaliongithub@gmail.com>
2023-09-13 23:23:04 +0530

    Initial Commit from VS Code

---
+++ src/App.js
@@ -0,0 +1,116 @@
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [prevOperand, setPrevOperand] = useState();
+    const [currentOperand, setCurrentOperand] = useState("0");
+    const [prevStatus, setPrevStatus] = useState([]);
+
+    const HandleBtnClick = (event) => {
+        // console.log(event.target.id === "del");
+
+        // if (event.target.id === "del") {
+        //     setCurrentOperand(prevStatus);
+        // }
+
+        // currentOperand && setPrevStatus(prevStatus?.push(currentOperand));
+
+        currentOperand !== "0"
+            ? setCurrentOperand(currentOperand + event?.target?.innerHTML)
+            : setCurrentOperand(event?.target?.innerHTML);
+    };
+
+    const HandleEq = (event) => {
+        if (event.target.id === "eq") {
+            const pattern = /([÷x+\-])/;
+            const arrOfInp = currentOperand?.split(pattern).filter(Boolean);
+            console.log(arrOfInp);
+            currentOperand &&
+                currentOperand !== "0" &&
+                setPrevOperand(currentOperand);
+        }
+    };
+
+    const HandleClear = (event) => {
+        if (event.target.id === "clear") {
+            setCurrentOperand("0");
+            // console.log("clear");
+            setPrevOperand("");
+        }
+    };
+
+    console.log("prevStatus", prevStatus);
+
+    return (
+        <div className="App">
+            <div className="calculator-grid">
+                {/* <div className="output-bg"> */}
+                <div className="output">
+                    <div className="prev-operand">{prevOperand}</div>
+                    <div className="current-operand anim-typewriter">
+                        {currentOperand}
+                    </div>
+                </div>
+                {/* </div> */}
+
+                <button onClick={HandleClear} id="clear" className="span-two">
+                    AC
+                </button>
+                <button onClick={HandleBtnClick} id="del">
+                    DEL
+                </button>
+                <button onClick={HandleBtnClick} id="div">
+                    ÷
+                </button>
+                <button onClick={HandleBtnClick} id="7">
+                    7
+                </button>
+                <button onClick={HandleBtnClick} id="8">
+                    8
+                </button>
+                <button onClick={HandleBtnClick} id="9">
+                    9
+                </button>
+                <button onClick={HandleBtnClick} id="prod">
+                    ×
+                </button>
+                <button onClick={HandleBtnClick} id="4">
+                    4
+                </button>
+                <button onClick={HandleBtnClick} id="5">
+                    5
+                </button>
+                <button onClick={HandleBtnClick} id="6">
+                    6
+                </button>
+                <button onClick={HandleBtnClick} id="sub">
+                    -
+                </button>
+                <button onClick={HandleBtnClick} id="1">
+                    1
+                </button>
+                <button onClick={HandleBtnClick} id="2">
+                    2
+                </button>
+                <button onClick={HandleBtnClick} id="3">
+                    3
+                </button>
+                <button onClick={HandleBtnClick} id="add">
+                    +
+                </button>
+                <button onClick={HandleBtnClick} id=".">
+                    .
+                </button>
+                <button onClick={HandleBtnClick} id="0">
+                    0
+                </button>
+                <button onClick={HandleEq} id="eq" className="span-two">
+                    =
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
